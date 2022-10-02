859b4687f55b942f5083e0d2f58b7dbf05c13b87
GmoZen <zendejas.guillermo@gmail.com>
2022-10-01 19:15:48 -0700

    update RecyclerView adapter file

--- app/src/main/java/com/example/flixter/MoviesRecyclerViewAdapter.kt
+++ app/src/main/java/com/example/flixter/MoviesRecyclerViewAdapter.kt
@@ -7,6 +7,8 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.TransformationUtils.centerInside
+import com.example.flixter.R.id
 
 
 
@@ -18,62 +20,58 @@ import com.bumptech.glide.Glide
 
 
 class MoviesRecyclerViewAdapter(
-    private val books: List<Movie>,
+    private val movies: List<Movie>,
     private val mListener: OnListFragmentInteractionListener?
 )
-//    : RecyclerView.Adapter<BestSellerBooksRecyclerViewAdapter.BookViewHolder>()
-//{
-//    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BookViewHolder {
-//        val view = LayoutInflater.from(parent.context)
-//            .inflate(R.layout.fragment_best_seller_book, parent, false)
-//        return BookViewHolder(view)
-//    }
-//
-//    /**
-//     * This inner class lets us refer to all the different View elements
-//     * (Yes, the same ones as in the XML layout files!)
-//     */
-//    inner class BookViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
-//        var mItem: BestSellerBook? = null
-//        val mBookTitle: TextView = mView.findViewById<View>(id.book_title) as TextView
-//        val mBookAuthor: TextView = mView.findViewById<View>(id.book_author) as TextView
-//        val mBookRanking: TextView = mView.findViewById<View>(id.ranking) as TextView
-//        val mBookDescription: TextView = mView.findViewById<View>(id.book_description) as TextView
-//        val mBookImage: ImageView = mView.findViewById<View>(id.book_image) as ImageView
-//
-//        override fun toString(): String {
-//            return mBookTitle.toString() + " '" + mBookAuthor.text + "'"
-//        }
-//    }
-//
-//    /**
-//     * This lets us "bind" each Views in the ViewHolder to its' actual data!
-//     */
-//    override fun onBindViewHolder(holder: BookViewHolder, position: Int) {
-//        val book = books[position]
-//
-//        holder.mItem = book
-//        holder.mBookTitle.text = book.title
-//        holder.mBookAuthor.text = book.author
-//        holder.mBookDescription.text = book.description
-//        holder.mBookRanking.text = book.rank.toString()
-//
-//        Glide.with(holder.mView)
-//            .load(book.bookImageUrl)
-//            .centerInside()
-//            .into(holder.mBookImage)
-//
-//        holder.mView.setOnClickListener {
-//            holder.mItem?.let { book ->
-//                mListener?.onItemClick(book)
-//            }
-//        }
-//    }
-//
-//    /**
-//     * Remember: RecyclerView adapters require a getItemCount() method.
-//     */
-//    override fun getItemCount(): Int {
-//        return books.size
-//    }
-//}
+    : RecyclerView.Adapter<MoviesRecyclerViewAdapter.MovieViewHolder>()
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieViewHolder {
+        val view = LayoutInflater.from(parent.context)
+            .inflate(R.layout.fragment_movie, parent, false)
+        return MovieViewHolder(view)
+    }
+
+    /**
+     * This inner class lets us refer to all the different View elements
+     * (Yes, the same ones as in the XML layout files!)
+     */
+    inner class MovieViewHolder(val mView: View) : RecyclerView.ViewHolder(mView) {
+        var mItem: Movie? = null
+        val mMovieTitle: TextView = mView.findViewById<View>(id.movie_title) as TextView
+        val mMovieOverview: TextView = mView.findViewById<View>(id.movie_overview) as TextView
+        val mMovieImage: ImageView = mView.findViewById<View>(id.movie_image) as ImageView
+
+        override fun toString(): String {
+            return mMovieTitle.toString()
+        }
+    }
+
+    /**
+     * This lets us "bind" each Views in the ViewHolder to its' actual data!
+     */
+    override fun onBindViewHolder(holder: MovieViewHolder, position: Int) {
+        val movie = movies[position]
+
+        holder.mItem = movie
+        holder.mMovieTitle.text = movie.title
+        holder.mMovieOverview.text = movie.overview
+
+        Glide.with(holder.mView)
+            .load("https://image.tmdb.org/t/p/w500/" + movie.movieImageUrl)
+            .centerInside()
+            .into(holder.mMovieImage)
+
+        holder.mView.setOnClickListener {
+            holder.mItem?.let { book ->
+                mListener?.onItemClick(book)
+            }
+        }
+    }
+
+    /**
+     * Remember: RecyclerView adapters require a getItemCount() method.
+     */
+    override fun getItemCount(): Int {
+        return movies.size
+    }
+}
