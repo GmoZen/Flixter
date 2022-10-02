d2bb8428a1a4398f9c1b386c0a2519918a8d9686
GmoZen <zendejas.guillermo@gmail.com>
2022-10-01 19:14:43 -0700

    update MoviesFragment.kt file

--- app/src/main/java/com/example/flixter/MoviesFragment.kt
+++ app/src/main/java/com/example/flixter/MoviesFragment.kt
@@ -16,12 +16,13 @@ import com.codepath.asynchttpclient.callback.JsonHttpResponseHandler
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
 import okhttp3.Headers
+import org.json.JSONArray
 import org.json.JSONObject
 
 
 
 
-private const val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+private const val API_KEY = "7785cc3db4f6c691eb73225be439c9b8"
 
 /*
  * The class for the only fragment in the app, which contains the progress bar,
@@ -30,101 +31,100 @@ private const val API_KEY = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
 
 
 class MoviesFragment : Fragment(), OnListFragmentInteractionListener {
-//
-//    /*
-//     * Constructing the view
-//     */
-//    override fun onCreateView(
-//        inflater: LayoutInflater, container: ViewGroup?,
-//        savedInstanceState: Bundle?
-//    ): View? {
-//        val view = inflater.inflate(R.layout.fragment_best_seller_books_list, container, false)
-//        val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
-//        val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
-//        val context = view.context
-//        recyclerView.layoutManager = GridLayoutManager(context, 2)
-//        updateAdapter(progressBar, recyclerView)
-//        return view
-//    }
-//
-//    /*
-//     * Updates the RecyclerView adapter with new data.  This is where the
-//     * networking magic happens!
-//     */
-//    private fun updateAdapter(progressBar: ContentLoadingProgressBar, recyclerView: RecyclerView) {
-//        progressBar.show()
-//
-//        // Create and set up an AsyncHTTPClient() here
-//        val client = AsyncHttpClient()
-//        val params = RequestParams()
-//        params["api-key"] = API_KEY
-//
-//        // Using the client, perform the HTTP request
-//        client[
-//                "https://api.nytimes.com/svc/books/v3/lists/current/hardcover-fiction.json",
-//                params,
-//                object : JsonHttpResponseHandler()
-//                // Uncomment me once you complete the above sections!
-//                {
-//                    /*
-//                     * The onSuccess function gets called when
-//                     * HTTP response status is "200 OK"
-//                     */
-//                    override fun onSuccess(
-//                        statusCode: Int,
-//                        headers: Headers,
-//                        json: JsonHttpResponseHandler.JSON
-//                    ) {
-//                        // The wait for a response is over
-//                        progressBar.hide()
-//
-//                        //TODO - Parse JSON into Models
-//
+
+    /*
+     * Constructing the view
+     */
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_movies_list, container, false)
+        val progressBar = view.findViewById<View>(R.id.progress) as ContentLoadingProgressBar
+        val recyclerView = view.findViewById<View>(R.id.list) as RecyclerView
+        val context = view.context
+        recyclerView.layoutManager = GridLayoutManager(context, 2)
+        updateAdapter(progressBar, recyclerView)
+        return view
+    }
+
+    /*
+     * Updates the RecyclerView adapter with new data.  This is where the
+     * networking magic happens!
+     */
+    private fun updateAdapter(progressBar: ContentLoadingProgressBar, recyclerView: RecyclerView) {
+        progressBar.show()
+
+        // Create and set up an AsyncHTTPClient() here
+        val client = AsyncHttpClient()
+        val params = RequestParams()
+        params["api_key"] = API_KEY
+
+        // Using the client, perform the HTTP request
+        client[
+                "https://api.themoviedb.org/3/movie/now_playing?",
+                params,
+                object : JsonHttpResponseHandler()
+                // Uncomment me once you complete the above sections!
+                {
+                    /*
+                     * The onSuccess function gets called when
+                     * HTTP response status is "200 OK"
+                     */
+                    override fun onSuccess(
+                        statusCode: Int,
+                        headers: Headers,
+                        json: JsonHttpResponseHandler.JSON
+                    ) {
+                        // The wait for a response is over
+                        progressBar.hide()
+
+                        //TODO - Parse JSON into Models
+
 //                        val resultsJSON : JSONObject = json.jsonObject.get("results") as JSONObject
-//                        val booksRawJSON : String = resultsJSON.get("books").toString()
-//                        val gson = Gson()
-//
-//                        val arrayBookType = object : TypeToken<List<BestSellerBook>>() {}.type
-//                        val models : List<BestSellerBook> = gson.fromJson(booksRawJSON, arrayBookType)
-//
-//
-//
-////                val models : List<BestSellerBook> = null // Fix me!
-//                        recyclerView.adapter = BestSellerBooksRecyclerViewAdapter(models, this@BestSellerBooksFragment)
-//
-//                        // Look for this in Logcat:
-//                        Log.d("BestSellerBooksFragment", "response successful")
-//                    }
-//
-//                    /*
-//                     * The onFailure function gets called when
-//                     * HTTP response status is "4XX" (eg. 401, 403, 404)
-//                     */
-//                    override fun onFailure(
-//                        statusCode: Int,
-//                        headers: Headers?,
-//                        errorResponse: String,
-//                        t: Throwable?
-//                    ) {
-//                        // The wait for a response is over
-//                        progressBar.hide()
-//
-//                        // If the error is not null, log it!
-//                        t?.message?.let {
-//                            Log.e("BestSellerBooksFragment", errorResponse)
-//                        }
-//                    }
-//                }]
-//        //
-//
-//    }
-//
-//    /*
-//     * What happens when a particular book is clicked.
-//     */
-//    override fun onItemClick(item: BestSellerBook) {
-//        Toast.makeText(context, "test: " + item.title, Toast.LENGTH_LONG).show()
-//    }
+                        val resultsJSON : JSONArray = json.jsonObject.getJSONArray("results") as JSONArray
+                        val moviesRawJSON : String = resultsJSON.toString()
+                        val gson = Gson()
+
+                        val arrayMovieType = object : TypeToken<List<Movie>>() {}.type
+                        val models : List<Movie> = gson.fromJson(moviesRawJSON, arrayMovieType)
+
+                        recyclerView.adapter = MoviesRecyclerViewAdapter(models, this@MoviesFragment)
+
+                        // Look for this in Logcat:
+                        Log.d("BestSellerBooksFragment", "response successful")
+                    }
+
+                    /*
+                     * The onFailure function gets called when
+                     * HTTP response status is "4XX" (eg. 401, 403, 404)
+                     */
+                    override fun onFailure(
+                        statusCode: Int,
+                        headers: Headers?,
+                        errorResponse: String,
+                        t: Throwable?
+                    ) {
+                        // The wait for a response is over
+                        progressBar.hide()
+
+
+                        // If the error is not null, log it!
+                        t?.message?.let {
+                            Log.e("MoviesFragment", errorResponse)
+                        }
+                    }
+                }]
+        //
+
+    }
+
+    /*
+     * What happens when a particular movie is clicked.
+     */
+    override fun onItemClick(item: Movie) {
+        Toast.makeText(context, "test: " + item.title, Toast.LENGTH_LONG).show()
+    }
 
 }
 
